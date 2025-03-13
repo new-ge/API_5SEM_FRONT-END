183989cec6de73e458752c9000c52bd62cfb5431
Raul Neto <rjbnetto@gmail.com>
2025-03-13 01:15:21 -0300

    VSN-12 feat: Create Operator Dashboard Screen

--- api_5sem_front-end/src/App.vue
+++ api_5sem_front-end/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TelaDashboardOperador />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TelaDashboardOperador from './components/TelaDashboardOperador.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      TelaDashboardOperador
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ api_5sem_front-end/src/components/TelaDashboardOperador.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+  <h1>Olá Mundo!</h1>
+  <p> Olá, {{ cargo }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TelaDashboardOperador',
+    data() {
+      return{
+        cargo: "Operador"
+      }
+    }
+  }
+</script>
